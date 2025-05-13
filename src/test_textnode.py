60e8d6ac825cb36641ef228dec227ff8400c7d02
Errett <errett.bentz@gmail.com>
2025-05-13 15:20:07 -0500

    added text_node_to_html_node func in main.py for converting text nodes to html nodes. Added some tests to test text_node_to_html_node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -12,7 +12,7 @@ class TestTextNode(unittest.TestCase):
     def test_eq2(self):
         text = "This is a text node"
         url = "www.google.com"
-        text_type = TextType.NORMAL
+        text_type = TextType.TEXT
         node = TextNode(text, text_type, url)
         node2 = TextNode(text, text_type, url)
         self.assertEqual(node, node2)
@@ -21,7 +21,7 @@ class TestTextNode(unittest.TestCase):
         text = "This is a text node"
         text2 = "This is a text node "
         url = "www.google.com"
-        text_type = TextType.NORMAL
+        text_type = TextType.TEXT
         node = TextNode(text, text_type, url)
         node2 = TextNode(text2, text_type, url)
         self.assertNotEqual(node, node2)
@@ -31,7 +31,7 @@ class TestTextNode(unittest.TestCase):
         text2 = "This is a text node"
         url = "google.com"
         url2 = "www.google.com"
-        text_type = TextType.NORMAL
+        text_type = TextType.TEXT
         node = TextNode(text, text_type, url2)
         node2 = TextNode(text2, text_type, url)
         self.assertNotEqual(node, node2)
@@ -41,7 +41,7 @@ class TestTextNode(unittest.TestCase):
         text2 = "This is a text node"
         url = "google.com"
         url2 = "www.google.com"
-        text_type = TextType.NORMAL
+        text_type = TextType.TEXT
         text_type2 = TextType.ITALIC
         node = TextNode(text, text_type, url2)
         node2 = TextNode(text2, text_type2, url)
---
+++ src/test_texttohtml.py
@@ -0,0 +1,58 @@
+import unittest
+
+from main import text_node_to_html_node
+from textnode import TextNode, TextType
+
+
+
+class TestTextToHtml(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        text_node = TextNode("This is a text node with bold text", TextType.BOLD)
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.tag, 'b')
+        self.assertEqual(html_node.value, "This is a text node with bold text")
+
+    def test_italic(self):
+        text_node = TextNode("This is a text node with italic text", TextType.ITALIC)
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.tag, 'i')
+        self.assertEqual(html_node.value, "This is a text node with italic text")
+
+    def test_code(self):
+        text_node = TextNode("This is a text node with code text", TextType.CODE)
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is a text node with code text")
+
+    def test_link(self):
+        tag = 'a'
+        anchor_text = "This is a text node with a link"
+        href_prop = {"href": "www.google.com"}
+        text_node = TextNode(anchor_text, TextType.LINK, href_prop["href"])
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.tag, tag)
+        self.assertEqual(html_node.value, anchor_text)
+        self.assertEqual(html_node.props, href_prop)
+
+    def test_image(self):
+        tag = "img"
+        html_node_value = ""
+        html_node_props = {"src": "/path/to/image.png", "alt": "alt text from text_node.text"}
+        text_node = TextNode(html_node_props["alt"], TextType.IMAGE, html_node_props["src"])
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.tag, tag)
+        self.assertEqual(html_node.value, html_node_value)
+        self.assertEqual(html_node.props, html_node_props)
+
+    def test_unknown_type(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
